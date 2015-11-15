8a534709c60d199c5cabd2d095f9c822af6ad2d6
jakubwasikowski <jakub.wasikowski@gmail.com>
2015-11-15 13:13:43 +0100

    Add sparse features representation, change the way how data set is splitted

--- features.py
+++ features.py
@@ -1,6 +1,7 @@
-import pandas as pd
+from collections import OrderedDict
 
-from encoding import encode_onehot
+import pandas as pd
+from scipy import sparse
 
 
 class FeatureSet:
@@ -17,16 +18,30 @@ class FeatureSet:
 
 class BasicFeatureSet(FeatureSet):
     def generate_features(self, data_set, features):
-        self._rewrite(data_set, features, ["Promo", "CompetitionDistance", "CompetitionOpenSinceMonth",
-                      "CompetitionOpenSinceYear", "Promo2", "Promo2SinceWeek", "Promo2SinceYear"], data_type=float)
-        # self._rewrite(data_set, features, ["Store", "DayOfWeek", "StoreType", "Assortment"], data_type=float)
+        self._rewrite(data_set, features, [
+            "Promo",
+            "Promo2",
+            "Promo2SinceWeek",
+            "Promo2SinceYear",
+            "CompetitionDistance",
+            "CompetitionOpenSinceMonth",
+            "CompetitionOpenSinceYear",
+            "SchoolHoliday",
+            "Store"
+        ], data_type=float)
+        self._rewrite(data_set, features, [
+            "DayOfWeek",
+            "StoreType",
+            "Assortment",
+            "StateHoliday"
+        ], data_type="category")
 
 
 class DateFeatureSet(FeatureSet):
     def generate_features(self, data_set, features):
-        features["Year"] = data_set.Date.apply(lambda x: x.split('-')[0]).astype(float)
-        features["Month"] = data_set.Date.apply(lambda x: x.split('-')[1]).astype(float)
-        features["Day"] = data_set.Date.apply(lambda x: x.split('-')[2]).astype(float)
+        features["Year"] = data_set.Date.apply(lambda x: x.split('-')[0]).astype("category")
+        features["Month"] = data_set.Date.apply(lambda x: x.split('-')[1]).astype("category")
+        features["Day"] = data_set.Date.apply(lambda x: x.split('-')[2]).astype("category")
 
 
 class FeaturesExtractor:
@@ -36,11 +51,59 @@ class FeaturesExtractor:
     def add_feature_set(self, feature_set):
         self._feature_set_generators.append(feature_set)
 
-    def extract(self, data_set):
+    def extract(self, data_set, feature_names=None):
+        feature_set = self._generate_feature_set(data_set)
+        return self._to_sparse_structure(feature_set, feature_names)
+
+    def _generate_feature_set(self, data_set):
         feature_set = pd.DataFrame(index=data_set.index)
         for fs_gen in self._feature_set_generators:
             fs_gen.generate_features(data_set, feature_set)
-        #cat_cols = [column for column in feature_set.columns.values if feature_set.dtypes[column].name == "category"]
-        #feature_set = encode_onehot(feature_set, cat_cols)
-
         return feature_set
+
+    def _to_sparse_structure(self, feature_set, feature_names):
+        categorical_columns, quantitative_columns = self._get_columns(feature_set)
+
+        row = []
+        col = []
+        data = []
+        if feature_names is not None:
+            features_indexes = OrderedDict((fn, i) for i, fn in enumerate(feature_names))
+        else:
+            features_indexes = OrderedDict()
+
+        rows_no = 0
+        for fs_index, fs_row in feature_set.iterrows():
+            for q_col in quantitative_columns:
+                self._update_feature_indexes(features_indexes, q_col)
+                row.append(rows_no)
+                col.append(features_indexes[q_col])
+                data.append(fs_row[q_col])
+            for c_col in categorical_columns:
+                one_hot_encoded_c_col = "%s=%s" % (c_col, str(fs_row[c_col]))
+                self._update_feature_indexes(features_indexes, one_hot_encoded_c_col)
+                row.append(rows_no)
+                col.append(features_indexes[one_hot_encoded_c_col])
+                data.append(1)
+            rows_no += 1
+
+        sparse_features = sparse.csr_matrix((data, (row, col)), shape=(rows_no, len(features_indexes)))
+        features_names = features_indexes.keys()
+        return sparse_features, features_names
+
+    def _update_feature_indexes(self, features_indexes, column):
+        if column not in features_indexes:
+            features_indexes[column] = len(features_indexes)
+
+    def _get_columns(self, feature_set):
+        categorical_columns = []
+        quantitative_columns = []
+        for column in feature_set.columns.values:
+            if feature_set.dtypes[column].name == "category":
+                categorical_columns.append(column)
+            else:
+                quantitative_columns.append(column)
+
+        return categorical_columns, quantitative_columns
+
+
--- runner.py
+++ runner.py
@@ -4,76 +4,120 @@ from os import path
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+from sklearn import cross_validation
 
 from cross_validation import non_random_train_test_split
 from features import FeaturesExtractor, BasicFeatureSet, DateFeatureSet
 
 
-params = {
-    "objective": "reg:linear",
-    "eta": 0.2,
-    "min_child_weight": 4,
-    "gamma": 5.0,
-    "max_depth": 10,
-    "subsample": 1.0,
-    "colsample_bytree": 1.0,
-    "silent": 1
-}
-
 # params = {
 #     "objective": "reg:linear",
-#     "eta": 0.3,
-#     "max_depth": 30,
-#     "subsample": 0.7,
-#     "colsample_bytree": 0.7,
+#     "eta": 0.2,
+#     "min_child_weight": 4,
+#     "gamma": 5.0,
+#     "max_depth": 8,
+#     "subsample": 1.0,
+#     "colsample_bytree": 1.0,
 #     "silent": 1
 # }
 
+params = {
+    "objective": "reg:linear",
+    "booster": "gbtree",
+    "eta": 0.1,
+    "max_depth": 10,
+    "subsample": 0.85,
+    "colsample_bytree": 0.4,
+    "min_child_weight": 6,
+    "silent": 1,
+    "seed": 1301
+}
+round_num = 1400
 
-def calc_rmspe(yhat, y):
-    rmspe = np.sqrt(np.mean(((y - yhat) / y)**2))
-    return rmspe
+
+def rmspe(yhat, y):
+    return np.sqrt(np.mean(((y - yhat)/y) ** 2))
 
 
-def calc_rmspe_xg(yhat, y):
-    # y = y.values
-    y = y.get_label()
-    y = np.exp(y) - 1
-    yhat = np.exp(yhat) - 1
-    rmspe = np.sqrt(np.mean(((y - yhat) / y)**2))
-    return "rmspe", rmspe
+def rmspe_xg(yhat, y):
+    y = np.expm1(y.get_label())
+    yhat = np.expm1(yhat)
+    return "rmspe", rmspe(yhat, y)
+
+
+# def median(training_set):
+#     columns = ['Store', 'DayOfWeek', 'Promo']
+#
+#     x_train, x_test, y_train, y_test = non_random_train_test_split(training_set, training_set.Sales, test_size=0.0388)
+#
+#     medians = x_train.groupby(columns)['Sales'].median()
+#     medians = medians.reset_index()
+#
+#     test2 = pd.merge(x_test, medians, on=columns, how='left')
+#     assert(len(test2) == len(x_test))
+#
+#     test2.loc[test2.Open == 0, 'Sales'] = 0
+#
+#     error = calc_rmspe(test2.Sales.values, y_test.values)
+#     print error
 
 
 def learning(features_extractor, training_set):
+    print ">> LEARNING"
+
     training_set.fillna(0, inplace=True)
+    training_set = training_set.loc[training_set["Sales"] > 0]
 
-    labels = training_set["Sales"]
-    features = features_extractor.extract(training_set)
+    print "Splitting data set..."
+    # train, valid = non_random_train_test_split(training_set, test_size=0.0388)
+    train, valid = cross_validation.train_test_split(training_set, test_size=0.012, random_state=10)
 
-    x_train, x_test, y_train, y_test = non_random_train_test_split(features, labels, test_size=0.0388)
+    print "Extracting features..."
+    train_x, train_names = features_extractor.extract(train)
+    train_y = train.Sales
 
-    d_train = xgb.DMatrix(x_train, np.log(y_train + 1))
-    d_valid = xgb.DMatrix(x_test, np.log(y_test + 1))
+    valid_x, _ = features_extractor.extract(valid, feature_names=train_names)
+    valid_y = valid.Sales
+
+    print "Feature names: %s" % ', '.join(train_names)
+
+    print "Training xgboost..."
+    d_train = xgb.DMatrix(train_x, label=np.log1p(train_y))
+    d_valid = xgb.DMatrix(valid_x, label=np.log1p(valid_y))
 
     watch_list = [(d_valid, 'eval'), (d_train, 'train')]
-    gbm = xgb.train(params, d_train, 2000, evals=watch_list, early_stopping_rounds=100,
-                    feval=calc_rmspe_xg)
+    model = xgb.train(params, d_train, round_num, evals=watch_list, early_stopping_rounds=200, feval=rmspe_xg)
 
-    print("Validating")
-    train_probs = gbm.predict(xgb.DMatrix(x_test))
+    print("Validating...")
+    train_probs = model.predict(xgb.DMatrix(valid_x))
     indices = train_probs < 0
     train_probs[indices] = 0
-    error = calc_rmspe(np.exp(train_probs) - 1, y_test.values)
+    error = rmspe(np.expm1(train_probs), valid_y.values)
     print "RMSPE error: %f" % error
+
+    return model, train_names
+
+
+def prediction(out_prediction_path, model, feature_names, features_extractor, test_set):
+    print ">> PREDICTION"
+
+    test_set.loc[test_set.Open.isnull(), 'Open'] = 1
+
+    print "Extracting features..."
+    test_x, _ = features_extractor.extract(test_set, feature_names=feature_names)
+
+    print "Predicting..."
+    test_probs = model.predict(xgb.DMatrix(test_x))
+    indices = test_probs < 0
+    test_probs[indices] = 0
+    submission = pd.DataFrame({"Id": test_set["Id"], "Sales": np.expm1(test_probs)})
+    submission.to_csv(out_prediction_path, index=False)
 
 
 def run(out_prediction_path, train_path, test_path, store_path):
     training_set = pd.read_csv(train_path)
     test_set = pd.read_csv(test_path)
     store = pd.read_csv(store_path)
-
-    training_set = training_set.loc[training_set["Sales"] > 0]
-    test_set.loc[test_set.Open.isnull(), 'Open'] = 1
 
     training_set = pd.merge(training_set, store, on="Store")
     test_set = pd.merge(test_set, store, on="Store")
@@ -82,7 +126,8 @@ def run(out_prediction_path, train_path, test_path, store_path):
     features_extractor.add_feature_set(BasicFeatureSet())
     features_extractor.add_feature_set(DateFeatureSet())
 
-    learning(features_extractor, training_set)
+    model, feature_names = learning(features_extractor, training_set)
+    prediction(out_prediction_path, model, feature_names, features_extractor, test_set)
 
 
 if __name__ == "__main__":
@@ -92,10 +137,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    train_path = path.join(args.input_dir, "train.csv")
-    test_path = path.join(args.input_dir, "test.csv")
-    store_path = path.join(args.input_dir, "store.csv")
-
-    out_prediction_path = path.join(args.output_dir, "predictions.csv")
-
-    run(out_prediction_path, train_path, test_path, store_path)
+    run(out_prediction_path=path.join(args.output_dir, "predictions.csv"),
+        train_path=path.join(args.input_dir, "train.csv"),
+        test_path=path.join(args.input_dir, "test.csv"),
+        store_path=path.join(args.input_dir, "store.csv"))
